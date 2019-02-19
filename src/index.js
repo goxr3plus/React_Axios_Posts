3c56c06ba9e7aa6e4522afcc56da84bdf204309f
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-19 13:25:12 +0200

    Adding Interceptors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+var requestIntercepton = axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    //Edit request config
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+var responseIntercepton = axios.interceptors.response.use(
+  response => {
+    console.log(response);
+    //Edit response config
+    return response;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+//Eject Interceptors ---- comment if your want to use them
+// axios.interceptors.request.eject(requestIntercepton);
+// axios.interceptors.response.eject(responseIntercepton);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
