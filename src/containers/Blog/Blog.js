361b661fc3863d4882d679dd92dcf26f43839af6
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-19 13:09:35 +0200

    First Code

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { Component } from "react";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import Post from "../../components/Post/Post";
+import "./Blog.css";
+
+class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  };
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
+  }
+
+  render() {
+    let posts = <p style={{ textAlign: "center" }}> Something went wrong!</p>;
+    if (!this.state.error)
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
