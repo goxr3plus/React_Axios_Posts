8860a6ed0d3210e37d8ab1f8a16d5168dca5b41b
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-20 12:03:45 +0200

    Added Loading Spinner

---
+++ src/components/UI/Spinner.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = props => <div className="Spinner" />;
+
+export default Spinner;
--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -1,36 +1,49 @@
 import axios from "axios";
 import React, { Component } from "react";
 import "./FullPost.css";
+import Spinner from "./../../../components/UI/Spinner";
 
 class FullPost extends Component {
   state = {
-    loadedPost: null
+    loadedPost: null,
+    postLaoding: false
   };
 
   deletePostHandler = () => {
-    axios
-      .delete("posts/" + this.props.id)
-      .then(response => {
-        console.log(response);
-      });
+    axios.delete("posts/" + this.props.id).then(response => {
+      console.log(response);
+    });
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.id)
+  componentDidMount(prevProps, prevState) {
+    console.log(this.props);
+    if (this.props.match.params.id)
       if (
         !this.state.loadedPost ||
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
-      )
+      ) {
+        this.setState({ postLaoding: true });
         axios
-          .get("posts/" + this.props.id)
+          .get("posts/" + this.props.match.params.id)
           .then(response => {
             this.setState({ loadedPost: response.data });
+            this.setState({ postLaoding: true });
+          })
+          .catch(error => {
+            this.setState({ postLaoding: false });
           });
+      }
   }
 
   render() {
+    console.log("ID " + this.props.match.params.id);
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-    if (this.props.id) post = <p style={{ textAlign: "center" }}>Loading...</p>;
+    if (this.props.match.params.id)
+      post = (
+        <p style={{ textAlign: "center" }}>
+          <Spinner />
+        </p>
+      );
     if (this.state.loadedPost) {
       post = (
         <div className="FullPost">
