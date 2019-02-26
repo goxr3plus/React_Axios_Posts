4024c6cdfa8aaaef36a5b1127343c1c6b7c4ab70
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-26 17:18:08 +0200

    Updates

---
+++ precache-manifest.8df9099338d7097c55399a71241371a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea6ee73f2f24b57958da",
+    "url": "/goxr3plus/React_Axios_Posts/static/css/main.7bc9208c.chunk.css"
+  },
+  {
+    "revision": "ea6ee73f2f24b57958da",
+    "url": "/goxr3plus/React_Axios_Posts/static/js/main.ea6ee73f.chunk.js"
+  },
+  {
+    "revision": "00bd3905084ca8c4d40b",
+    "url": "/goxr3plus/React_Axios_Posts/static/js/runtime~main.00bd3905.js"
+  },
+  {
+    "revision": "d7b3757bbb033c6b80b5",
+    "url": "/goxr3plus/React_Axios_Posts/static/js/2.d7b3757b.chunk.js"
+  },
+  {
+    "revision": "f536c99d149b0abf2343",
+    "url": "/goxr3plus/React_Axios_Posts/static/css/3.45c57064.chunk.css"
+  },
+  {
+    "revision": "f536c99d149b0abf2343",
+    "url": "/goxr3plus/React_Axios_Posts/static/js/3.f536c99d.chunk.js"
+  },
+  {
+    "revision": "e042475c30ef9472bd9f0d0240eed155",
+    "url": "/goxr3plus/React_Axios_Posts/index.html"
+  }
+];
